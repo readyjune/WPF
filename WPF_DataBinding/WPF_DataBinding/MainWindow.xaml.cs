8a2f7fbe4b16625c4225e41cbc2238100ead8270
Jinwoo Kim <jinwoo.kim@student.curtin.edu.au>
2023-07-26 12:43:32 +0900

    Add files via upload

---
+++ WPF_DataBinding/WPF_DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_DataBinding
+{
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set
+            {
+                boundText = value;
+               
+
+                OnPropertyChanged();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+        }
+    }
+
+}
